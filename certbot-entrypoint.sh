9ccbe7101ea1ad46dccc0675beaa6e48e1434b54
David Du <david2001.du@gmail.com>
2025-03-03 18:51:16 -0800

    Fix certbot deploy-hook with docker-compose exec

--- certbot-entrypoint.sh
+++ certbot-entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-# Initial certificate request
-certbot certonly --webroot --webroot-path=/var/www/letsencrypt \
-  --email moderatelyburnedpotato@gmail.com --agree-tos --no-eff-email \
-  -d hao123.ddns.net --non-interactive
+
+# Initial certificate request (only runs once, unless the cert is missing)
+if [ ! -f /etc/letsencrypt/live/hao123.ddns.net/fullchain.pem ]; then
+  certbot certonly --webroot --webroot-path=/var/www/letsencrypt \
+    --email moderatelyburnedpotato@gmail.com --agree-tos --no-eff-email \
+    -d hao123.ddns.net --non-interactive
+fi
 
 # Renewal loop
 while true; do
-  certbot renew --webroot --webroot-path=/var/www/letsencrypt \
-    --deploy-hook "nginx -s reload" --quiet
+  certbot renew --webroot --webroot-path=/var/www/letsencrypt --quiet \
+    --deploy-hook "docker-compose -f /home/ubuntu/website-nginx/docker-compose.yml exec -T nginx nginx -s reload"
   sleep 12h
 done
+
